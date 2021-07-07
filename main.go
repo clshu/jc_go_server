c2388ea702c5eff3139a24db10429128c112815a
clshu <ccss0630@yahoo.com>
2021-07-06 20:29:15 -0700

    3.10_Adding_a_404_page

--- main.go
+++ main.go
@@ -22,11 +22,16 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<p>Where to contact <a href=\"mailto:support@lenslock.com\">support@lenslock.com</a></p><p>Address: 555 main st., Owen City, CA 99999</p>")
 }
 
+func noFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Sorry we can not find the page</h1><p> If it continue to happen, contact <a href=\"mailto:support@lenslock.com\">support@lenslock.com</a></p>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
-
+	r.NotFoundHandler = http.HandlerFunc(noFound)
 	_ = http.ListenAndServe(":3000", r)
 }
