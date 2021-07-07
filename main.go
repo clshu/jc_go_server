b2d756722bbb561829410ea9615c60967632e264
clshu <ccss0630@yahoo.com>
2021-07-06 20:45:50 -0700

    3.11_Exercises_using_httprouter reouter.NotFound vs r.NotFoundHandler

--- main.go
+++ main.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome website</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<p>To get in touch, contact <a href=\"mailto:support@lenslock.com\">support@lenslock.com</a></p>")
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<p>Where to contact <a href=\"mailto:support@lenslock.com\">support@lenslock.com</a></p><p>Address: 555 main st., Owen City, CA 99999</p>")
 }
@@ -28,10 +28,10 @@ func noFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	r.NotFoundHandler = http.HandlerFunc(noFound)
-	_ = http.ListenAndServe(":3000", r)
+	router := httprouter.New()
+	router.GET("/", home)
+	router.GET("/contact", contact)
+	router.GET("/faq", faq)
+	router.NotFound = http.HandlerFunc(noFound)
+	_ = http.ListenAndServe(":3000", router)
 }
