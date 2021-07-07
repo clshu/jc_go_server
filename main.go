5b52f0a5f7426d177f5e58d0849a68e71db41cb2
clshu <ccss0630@yahoo.com>
2021-07-07 01:34:02 -0700

    6.1_Creating_1st_view home page.

--- main.go
+++ main.go
@@ -2,36 +2,46 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 )
 
-func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+var homeTemplate *template.Template
+
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome website</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
-func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<p>To get in touch, contact <a href=\"mailto:support@lenslock.com\">support@lenslock.com</a></p>")
 }
 
-func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<p>Where to contact <a href=\"mailto:support@lenslock.com\">support@lenslock.com</a></p><p>Address: 555 main st., Owen City, CA 99999</p>")
 }
 
-func noFound(w http.ResponseWriter, r *http.Request) {
+func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Sorry we can not find the page</h1><p> If it continue to happen, contact <a href=\"mailto:support@lenslock.com\">support@lenslock.com</a></p>")
 }
 
 func main() {
-	router := httprouter.New()
-	router.GET("/", home)
-	router.GET("/contact", contact)
-	router.GET("/faq", faq)
-	router.NotFound = http.HandlerFunc(noFound)
-	_ = http.ListenAndServe(":3000", router)
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	_ = http.ListenAndServe(":3000", r)
 }
