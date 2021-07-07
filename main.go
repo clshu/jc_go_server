d6db50c65dc1504742965a5fb36972ec36693dbc
clshu <ccss0630@yahoo.com>
2021-07-06 20:08:30 -0700

    3.7_Implementing_gorilla_mux

--- main.go
+++ main.go
@@ -19,10 +19,19 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome website</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<p>To get in touch, contact <a href=\"mailto:support@lenslock.com\">support@lenslock.com</a></p>")
+}
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlerFunc)
-	r.HandleFunc("/contact", handlerFunc)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 
 	_ = http.ListenAndServe(":3000", r)
 }
