79269754b7ae6acfb763400a089895f0763bb5c6
clshu <ccss0630@yahoo.com>
2021-07-06 18:52:52 -0700

    3.3_Adding_a_404_page

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my awesome website</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "<p>To get in touch, contact <a href=\"mailto:support@lenslock.com\">support@lenslock.com</a></p>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>Sorry we can not find the page</h1><p>contact us if you continue having the problem</p>")
 	}
 }
 
