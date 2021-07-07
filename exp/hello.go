c5bb10f4524b29418ecbd91e0c46367347f0e7d9
clshu <ccss0630@yahoo.com>
2021-07-06 21:17:57 -0700

    Add exp dir to experiemnt

---
+++ exp/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	data := User{
+		Name: "John Smith",
+	}
+
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
+
+	data.Name = "Bobby Jones"
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
+}
