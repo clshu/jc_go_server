94bc72998dc094c5ed4fd0ce0a5b6a7e5bf3f1bc
clshu <ccss0630@yahoo.com>
2021-07-09 03:14:55 -0700

    7.8_Creating_a_controller_for_mostly_static_pages7.8_Creating_a_controller_for_mostly_static_pages

--- main.go
+++ main.go
@@ -4,37 +4,16 @@ import (
 	"net/http"
 
 	"github.com/clshu/jc_go_server/controllers"
-	"github.com/clshu/jc_go_server/views"
 	"github.com/gorilla/mux"
 )
 
-var (
-	homeView    *views.View
-	contactView *views.View
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if err := homeView.Render(w, nil); err != nil {
-		panic(err)
-	}
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if err := contactView.Render(w, nil); err != nil {
-		panic(err)
-	}
-}
-
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	staticC := controllers.NewStatic()
 	usersC := controllers.NewUser()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 
